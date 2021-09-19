1b02e4c96058579e728db1f1370cbd7aa47369d3
shubhamshriwastwa <91003012+shubhamshriwastwa@users.noreply.github.com>
2021-09-19 15:26:02 +0530

    Create SS_06_shubh.java

---
+++ SS_06_shubh.java
@@ -0,0 +1,10 @@
+
+public class SS_06_shubh {
+    public static void main(String[] args) {
+        int[] marks = {55,54,67,70,100,120};
+        for(int i=0;i<marks.length;i++) {
+            System.out.print((char)marks[i]+" ");
+        }
+
+    }
+}
